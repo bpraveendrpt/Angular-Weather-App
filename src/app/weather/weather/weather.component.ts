560a06d744afd295792cfdf0c6508bcfb0a9154d
Praveen kumar Bandi <bpraveendrpt@gmail.com>
2023-11-16 21:43:23 -0500

    weather API

--- src/app/weather/weather/weather.component.ts
+++ src/app/weather/weather/weather.component.ts
@@ -11,5 +11,7 @@ export class WeatherComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  // API KEY AND URL
+  //const apiKey = '626d99c5df067c8d642c15ab3dfbd2a3';
+  // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
 }
